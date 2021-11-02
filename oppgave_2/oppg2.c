2d78504878b36178ca25b78429a6a12402958124
magnuen2k <magnuen2k@gmail.com>
2021-11-02 15:12:05 +0100

    read from file and convert

--- oppgave_2/oppg2.c
+++ oppgave_2/oppg2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int hex_to_int(char c){
@@ -19,28 +20,39 @@ int hex_to_ascii(char c, char d){
 
 int main(int argc, char *argv[]) {
 
-    char *st = "446574746520657220656e2066696c206465726520736b616c207465737465206d65642c2064656e20696e6e65686f6c6465722074656b737420736f6d2065722068657820656e6b6f6465742e2044656e6e65206f7070676176656e20696c6c757374726572657220656e6b656c206c6573696e672061762066696c6572206f672061742064657265206b616e20746120656e20656e6b656c206b6f6e766572746572696e6720617620646174612c2073616d7420656e6b656c206c6f6f70206b6f64652e204465747465206b6c61726572206475206e6f6b2066696e742e";
+    FILE *f = NULL;
+    unsigned char *byBuffer = NULL;
+    long lSize = 0, lBytesRead;
 
-	/*FILE *f;
-	char c;
-	f=fopen("data.dta","r");
-	while((c=fgetc(f))!=EOF)
-	{
-		printf("%c",c);
-	}
-	fclose(f);*/
+    f = fopen ("data.dta", "r"); // Binary read. "r" for text would work too.
+    if (f != NULL) {
+        if (fseek(f, 0, SEEK_END) == 0) {
+            lSize = ftell(f);
+            printf ("Size of file is %ld bytes.\n", lSize);
+            rewind(f);
 
-    int length = strlen(st);
-
-    int i;
-        char buf = 0;
-        for(i = 0; i < length; i++){
-            if(i % 2 != 0){
-                printf("%c", hex_to_ascii(buf, st[i]));
-            } else{
-                buf = st[i];
+            byBuffer = malloc (lSize);
+            if (byBuffer != NULL) {
+                lBytesRead = fread (byBuffer, sizeof(unsigned char), lSize, f);
+                if (lBytesRead == lSize) {
+                    // We got it all:
+                    //printf("Buffer: %s", byBuffer);
+                    int length = strlen(byBuffer);
+                    int i;
+                    char buf = 0;
+                    for(i = 0; i < length; i++){
+                        if(i % 2 != 0){
+                            printf("%c", hex_to_ascii(buf, byBuffer[i]));
+                        } else{
+                            buf = byBuffer[i];
+                        }
+                    }
+                }
+                free (byBuffer);
             }
         }
+        fclose (f);
+    }
 
 	return 0;
 
