c241e02980d52ec83c228ac38e94b437847fff9b
magnuen2k <magnuen2k@gmail.com>
2021-11-02 15:20:39 +0100

    writing converted string into text file

--- oppgave_2/oppg2.c
+++ oppgave_2/oppg2.c
@@ -20,11 +20,11 @@ int hex_to_ascii(char c, char d){
 
 int main(int argc, char *argv[]) {
 
-    FILE *f = NULL;
+    FILE *f = NULL, *ft;
     unsigned char *byBuffer = NULL;
     long lSize = 0, lBytesRead;
 
-    f = fopen ("data.dta", "r"); // Binary read. "r" for text would work too.
+    f = fopen ("data.dta", "r");
     if (f != NULL) {
         if (fseek(f, 0, SEEK_END) == 0) {
             lSize = ftell(f);
@@ -33,22 +33,23 @@ int main(int argc, char *argv[]) {
 
             byBuffer = malloc (lSize);
             if (byBuffer != NULL) {
-                lBytesRead = fread (byBuffer, sizeof(unsigned char), lSize, f);
-                if (lBytesRead == lSize) {
-                    // We got it all:
-                    //printf("Buffer: %s", byBuffer);
-                    int length = strlen(byBuffer);
-                    int i;
-                    char buf = 0;
-                    for(i = 0; i < length; i++){
-                        if(i % 2 != 0){
-                            printf("%c", hex_to_ascii(buf, byBuffer[i]));
-                        } else{
-                            buf = byBuffer[i];
+                ft = fopen("text.txt", "w");
+                if(ft != NULL) {
+                    lBytesRead = fread (byBuffer, sizeof(unsigned char), lSize, f);
+                    if (lBytesRead == lSize) {
+                        int i;
+                        char buf = 0;
+                        for(i = 0; i < strlen(byBuffer); i++){
+                            if(i % 2 != 0){
+                                fprintf(ft, "%c", hex_to_ascii(buf, byBuffer[i]));
+                            } else{
+                                buf = byBuffer[i];
+                            }
                         }
                     }
                 }
                 free (byBuffer);
+                fclose(ft);
             }
         }
         fclose (f);
